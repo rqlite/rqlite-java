53757dce046f592bd789c3465d8dfc07f2e3d3c3
Philip O'Toole <philip.otoole@yahoo.com>
2017-08-09 22:30:28 -0700

    Formatting post Eclipse-config

--- src/main/java/com/rqlite/rqlite.java
+++ src/main/java/com/rqlite/rqlite.java
@@ -1,24 +1,22 @@
 package com.rqlite;
 
 public interface rqlite {
-	
-	public enum ReadConsistencyLevel {
-		NONE("none"),
-		WEAK("weak"),
-		STRONG("strong");
-		
-		private String value;
-		
-	    private ReadConsistencyLevel(String value) {
-	        this.value = value;
-	    }
 
-	    public String value() {
-	        return this.value;
-	    }		
-	}
-	
-	public void Query(String q);
-	
-	public void Execute(String q);
+    public enum ReadConsistencyLevel {
+        NONE("none"), WEAK("weak"), STRONG("strong");
+
+        private final String value;
+
+        private ReadConsistencyLevel(final String value) {
+            this.value = value;
+        }
+
+        public String value() {
+            return this.value;
+        }
+    }
+
+    public void Query(String q);
+
+    public void Execute(String q);
 }
