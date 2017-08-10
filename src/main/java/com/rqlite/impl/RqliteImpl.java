80fcec364dad0592a631d97bb4e82549b4e0703f
Philip O'Toole <philip.otoole@yahoo.com>
2017-08-09 23:11:43 -0700

    Add skeleton of implementation

---
+++ src/main/java/com/rqlite/impl/RqliteImpl.java
@@ -0,0 +1,17 @@
+package com.rqlite.impl;
+
+import com.rqlite.Rqlite;
+
+public class RqliteImpl implements Rqlite {
+
+    public void Query(String q, boolean tx, ReadConsistencyLevel lvl) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void Execute(String q) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
