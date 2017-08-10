c1a9dec912cfbcf688f3122f5555d7699253c143
Philip O'Toole <philip.otoole@yahoo.com>
2017-08-09 23:44:19 -0700

    Add rqlite factory

---
+++ src/main/java/com/rqlite/RqliteFactory.java
@@ -0,0 +1,18 @@
+package com.rqlite;
+
+import com.rqlite.impl.RqliteImpl;
+
+public enum RqliteFactory {
+    INSTANCE; // XXX read about this! Enum-based factories.
+
+    /**
+     * Create a connection to a rqlite node.
+     *
+     * @param url
+     *            the URL to connect to.
+     * @return a rqlite client instance.
+     */
+    public static Rqlite connect(final String url) {
+        return new RqliteImpl();
+    }
+}
