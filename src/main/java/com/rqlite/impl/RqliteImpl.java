10b9efad777b890ea7039dbe580f67a1694fb6e6
Philip O'Toole <philip.otoole@yahoo.com>
2017-08-09 23:44:06 -0700

    More work in impl

--- src/main/java/com/rqlite/impl/RqliteImpl.java
+++ src/main/java/com/rqlite/impl/RqliteImpl.java
@@ -1,10 +1,35 @@
 package com.rqlite.impl;
 
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.rqlite.Rqlite;
 
 public class RqliteImpl implements Rqlite {
 
+    static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    private final HttpRequestFactory requestFactory;
+    private final String url;
+
+    public RqliteImpl(final String host, final Integer port) {
+        this.url = "http://" + host + ":" + port;
+
+        this.requestFactory = HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
+            public void initialize(HttpRequest request) {
+                request.setParser(new JsonObjectParser(JSON_FACTORY));
+            }
+        });
+    }
+
     public void Query(String q, boolean tx, ReadConsistencyLevel lvl) {
+        HttpRequest request = this.requestFactory.buildGetRequest("xxx"); // XXX needs inherit from generic URL
         // TODO Auto-generated method stub
 
     }
