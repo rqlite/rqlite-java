fbfcb277181ec1803811799b857f1083c5bf0f0b
Philip O'Toole <philip.otoole@yahoo.com>
2017-08-13 17:11:44 -0700

    Add Pong type

---
+++ src/main/java/com/rqlite/Pong.java
@@ -0,0 +1,13 @@
+package com.rqlite;
+
+public class Pong {
+    public String version;
+
+    public Pong() {
+        this.version = "unknown";
+    }
+
+    public Pong(final String version) {
+        this.version = version;
+    }
+}
