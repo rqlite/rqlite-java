add9c684f49167615864dbb48c1904698313e738
Philip O'Toole <philip.otoole@yahoo.com>
2017-08-26 22:34:44 -0700

    Multi-statement unit testing

--- src/test/java/com/rqlite/RqliteClientTest.java
+++ src/test/java/com/rqlite/RqliteClientTest.java
@@ -28,6 +28,23 @@ public class RqliteClientTest {
 
     @Test
     public void testRqliteClientMulti() {
+        Rqlite rqlite = RqliteFactory.connect("http", "localhost", 4001);
+        ExecuteResults results = null;
+        QueryResults rows = null;
+
+        results = rqlite.Execute("CREATE TABLE foo (id integer not null primary key, name text)");
+        Assert.assertNotNull(results);
+        System.out.println(results.toString());
+
+        String[] s = { "INSERT INTO foo(name) VALUES(\"fiona\")", "INSERT INTO foo(name) VALUES(\"declan\")" };
+        results = rqlite.Execute(s, false);
+        Assert.assertNotNull(results);
+        System.out.println(results.toString());
+
+        String[] q = { "SELECT * FROM foo", "SELECT name FROM foo" };
+        rows = rqlite.Query(q, false, Rqlite.ReadConsistencyLevel.WEAK);
+        Assert.assertNotNull(rows);
+        System.out.println(rows.toString());
     }
 
     @Test
