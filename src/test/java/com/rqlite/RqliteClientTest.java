e9476d1ba0b5b541abc149714c6669b7e060386a
Philip O'Toole <philip.otoole@yahoo.com>
2017-10-01 21:27:56 -0700

    Add more unit-testing
    The required use of BigDecimal still needs investigation.

--- src/test/java/com/rqlite/RqliteClientTest.java
+++ src/test/java/com/rqlite/RqliteClientTest.java
@@ -1,5 +1,7 @@
 package com.rqlite;
 
+import java.math.BigDecimal;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,7 +18,6 @@ public class RqliteClientTest {
         results = rqlite.Execute("CREATE TABLE foo (id integer not null primary key, name text)");
         Assert.assertNotNull(results);
         Assert.assertEquals(1, results.results.length);
-        Assert.assertEquals(0, results.results[0].lastInsertId);
 
         results = rqlite.Execute("INSERT INTO foo(name) VALUES(\"fiona\")");
         Assert.assertNotNull(results);
@@ -24,15 +25,12 @@ public class RqliteClientTest {
         Assert.assertEquals(1, results.results[0].lastInsertId);
 
         rows = rqlite.Query("SELECT * FROM foo", Rqlite.ReadConsistencyLevel.WEAK);
-        System.out.println(rows.toString());
-
         Assert.assertNotNull(rows);
         Assert.assertEquals(1, rows.results.length);
         Assert.assertArrayEquals(new String[] { "id", "name" }, rows.results[0].columns);
         Assert.assertArrayEquals(new String[] { "integer", "text" }, rows.results[0].types);
         Assert.assertEquals(1, rows.results[0].values.length);
-        // Assert.assertArrayEquals(new Object[] { 1, "fiona" },
-        // rows.results[0].values[0]);
+        Assert.assertArrayEquals(new Object[] { new BigDecimal(1), "fiona" }, rows.results[0].values[0]);
     }
 
     @Test
@@ -41,19 +39,36 @@ public class RqliteClientTest {
         ExecuteResults results = null;
         QueryResults rows = null;
 
-        results = rqlite.Execute("CREATE TABLE foo (id integer not null primary key, name text)");
+        results = rqlite.Execute("CREATE TABLE bar (id integer not null primary key, name text)");
         Assert.assertNotNull(results);
-        System.out.println(results.toString());
+        Assert.assertEquals(1, results.results.length);
 
-        String[] s = { "INSERT INTO foo(name) VALUES(\"fiona\")", "INSERT INTO foo(name) VALUES(\"declan\")" };
+        String[] s = { "INSERT INTO bar(name) VALUES(\"fiona\")", "INSERT INTO bar(name) VALUES(\"declan\")" };
         results = rqlite.Execute(s, false);
         Assert.assertNotNull(results);
-        System.out.println(results.toString());
+        Assert.assertEquals(2, results.results.length);
+        Assert.assertEquals(1, results.results[0].lastInsertId);
+        Assert.assertEquals(2, results.results[1].lastInsertId);
 
-        String[] q = { "SELECT * FROM foo", "SELECT name FROM foo" };
+        String[] q = { "SELECT * FROM bar", "SELECT name FROM bar" };
         rows = rqlite.Query(q, false, Rqlite.ReadConsistencyLevel.WEAK);
         Assert.assertNotNull(rows);
-        System.out.println(rows.toString());
+        Assert.assertNotNull(rows);
+        Assert.assertEquals(2, rows.results.length);
+
+        // SELECT * FROM bar
+        Assert.assertArrayEquals(new String[] { "id", "name" }, rows.results[0].columns);
+        Assert.assertArrayEquals(new String[] { "integer", "text" }, rows.results[0].types);
+        Assert.assertEquals(2, rows.results[0].values.length);
+        Assert.assertArrayEquals(new Object[] { new BigDecimal(1), "fiona" }, rows.results[0].values[0]);
+        Assert.assertArrayEquals(new Object[] { new BigDecimal(2), "declan" }, rows.results[0].values[1]);
+
+        // SELECT name FROM bar
+        Assert.assertArrayEquals(new String[] { "name" }, rows.results[1].columns);
+        Assert.assertArrayEquals(new String[] { "text" }, rows.results[1].types);
+        Assert.assertEquals(2, rows.results[1].values.length);
+        Assert.assertArrayEquals(new Object[] { "fiona" }, rows.results[1].values[0]);
+        Assert.assertArrayEquals(new Object[] { "declan" }, rows.results[1].values[1]);
     }
 
     @Test
@@ -64,10 +79,13 @@ public class RqliteClientTest {
 
         results = rqlite.Execute("nonsense");
         Assert.assertNotNull(results);
-        System.out.println(results.toString());
+        Assert.assertEquals(1, results.results.length);
+        Assert.assertEquals(0, results.results[0].rowsAffected);
+        Assert.assertEquals("near \"nonsense\": syntax error", results.results[0].error);
 
         rows = rqlite.Query("more nonsense", Rqlite.ReadConsistencyLevel.WEAK);
         Assert.assertNotNull(rows);
-        System.out.println(rows.toString());
+        Assert.assertEquals(1, rows.results.length);
+        Assert.assertEquals("near \"more\": syntax error", rows.results[0].error);
     }
 }
