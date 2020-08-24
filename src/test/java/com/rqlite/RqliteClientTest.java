267776c33c789197ac471d4e9e9057d3a4fe92e5
Hannah Provenza <hprovenza@gmail.com>
2020-08-24 14:28:09 -0400

    Added teardown using @after

--- src/test/java/com/rqlite/RqliteClientTest.java
+++ src/test/java/com/rqlite/RqliteClientTest.java
@@ -9,8 +9,10 @@ import com.rqlite.dto.ExecuteResults;
 import com.rqlite.dto.QueryResults;
 
 public class RqliteClientTest {
+
     @Test
     public void testRqliteClientSingle() {
+
         Rqlite rqlite = RqliteFactory.connect("http", "localhost", 4001);
         ExecuteResults results = null;
         QueryResults rows = null;
@@ -31,52 +33,51 @@ public class RqliteClientTest {
         Assert.assertArrayEquals(new String[] { "integer", "text" }, rows.results[0].types);
         Assert.assertEquals(1, rows.results[0].values.length);
         Assert.assertArrayEquals(new Object[] { new BigDecimal(1), "fiona" }, rows.results[0].values[0]);
-        
-        rqlite.Execute("DROP TABLE foo");
     }
 
     @Test
     public void testRqliteClientMulti() {
+
         Rqlite rqlite = RqliteFactory.connect("http", "localhost", 4001);
         ExecuteResults results = null;
         QueryResults rows = null;
 
-        results = rqlite.Execute("CREATE TABLE bar (id integer not null primary key, name text)");
+        results = rqlite.Execute("CREATE TABLE foo (id integer not null primary key, name text)");
         Assert.assertNotNull(results);
         Assert.assertEquals(1, results.results.length);
 
-        String[] s = { "INSERT INTO bar(name) VALUES(\"fiona\")", "INSERT INTO bar(name) VALUES(\"declan\")" };
+        String[] s = { "INSERT INTO foo(name) VALUES(\"fiona\")", "INSERT INTO foo(name) VALUES(\"declan\")" };
         results = rqlite.Execute(s, false);
         Assert.assertNotNull(results);
         Assert.assertEquals(2, results.results.length);
         Assert.assertEquals(1, results.results[0].lastInsertId);
         Assert.assertEquals(2, results.results[1].lastInsertId);
 
-        String[] q = { "SELECT * FROM bar", "SELECT name FROM bar" };
+        String[] q = { "SELECT * FROM foo", "SELECT name FROM foo" };
         rows = rqlite.Query(q, false, Rqlite.ReadConsistencyLevel.WEAK);
         Assert.assertNotNull(rows);
         Assert.assertNotNull(rows);
         Assert.assertEquals(2, rows.results.length);
 
-        // SELECT * FROM bar
+        // SELECT * FROM foo
         Assert.assertArrayEquals(new String[] { "id", "name" }, rows.results[0].columns);
         Assert.assertArrayEquals(new String[] { "integer", "text" }, rows.results[0].types);
         Assert.assertEquals(2, rows.results[0].values.length);
         Assert.assertArrayEquals(new Object[] { new BigDecimal(1), "fiona" }, rows.results[0].values[0]);
         Assert.assertArrayEquals(new Object[] { new BigDecimal(2), "declan" }, rows.results[0].values[1]);
 
-        // SELECT name FROM bar
+        // SELECT name FROM foo
         Assert.assertArrayEquals(new String[] { "name" }, rows.results[1].columns);
         Assert.assertArrayEquals(new String[] { "text" }, rows.results[1].types);
         Assert.assertEquals(2, rows.results[1].values.length);
         Assert.assertArrayEquals(new Object[] { "fiona" }, rows.results[1].values[0]);
         Assert.assertArrayEquals(new Object[] { "declan" }, rows.results[1].values[1]);
-        
-        rqlite.Execute("DROP TABLE bar");
+	rqlite.Execute("DROP TABLE foo");
     }
 
     @Test
     public void testRqliteClientSyntax() {
+
         Rqlite rqlite = RqliteFactory.connect("http", "localhost", 4001);
         ExecuteResults results = null;
         QueryResults rows = null;
@@ -91,5 +92,12 @@ public class RqliteClientTest {
         Assert.assertNotNull(rows);
         Assert.assertEquals(1, rows.results.length);
         Assert.assertEquals("near \"more\": syntax error", rows.results[0].error);
+    }
+
+    @After
+    public void after() throws Exception {
+        Rqlite rqlite = RqliteFactory.connect("http", "localhost", 4001);
+        rqlite.Execute("DROP TABLE foo");
+
     }
 }
